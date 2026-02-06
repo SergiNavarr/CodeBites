300ccf4146491de4e1fa09fc6de1481b5e5ec5b9
Sergio Navarro <nachonavarro197@gmail.com>
2026-02-06 00:25:08 -0300

    fix margin logo

--- Frontend/components/global-navbar.tsx
+++ Frontend/components/global-navbar.tsx
@@ -43,7 +43,7 @@ export function GlobalNavbar() {
         <div className="container mx-auto flex h-16 items-center justify-between px-4">
           
           <Link href="/" className="flex items-center gap-3">
-            <div className="relative h-8 w-10">
+            <div className="relative h-8 w-10 -mr-2">
               <Image
                 src="/logo-bites.png"
                 alt="CodeBites Logo"
